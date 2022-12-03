88732624968af38900f81bf5e25b06b90244a67e
IgorScotti <igorscoti@hotmail.com>
2022-12-03 17:07:14 -0300

    Criando a entrada das notas

--- src/application/Program_Media_Alunos.java
+++ src/application/Program_Media_Alunos.java
@@ -14,7 +14,7 @@ public class Program_Media_Alunos {
 		double vectNotas[] = new double[numero_notas];
 
 		for (int i = 0; i < vectNotas.length; i++) {
-			System.out.print("Digite a " + (i + 1) + " Nota: ");
+			System.out.print("Digite a " + (i + 1) + " Nota do aluno: ");
 			int nota = sc.nextInt();
 			vectNotas[i] = nota;
 		}
